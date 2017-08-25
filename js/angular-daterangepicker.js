e051c334c599ba0ad118e310aa7a673a06530c94
Guillermo Peralta Scura <gperaltascura@gmail.com>
2017-08-25 10:57:40 -0400

    Model doesn't update for current day

--- js/angular-daterangepicker.js
+++ js/angular-daterangepicker.js
@@ -37,7 +37,7 @@
         };
         el = $(element);
         customOpts = $scope.opts;
-        opts = _mergeOpts({}, dateRangePickerConfig, customOpts);
+        opts = _mergeOpts({}, angular.copy(dateRangePickerConfig), customOpts);
         _picker = null;
         _clear = function() {
           _picker.setStartDate();
@@ -90,7 +90,7 @@
           };
           if (opts.singleDatePicker && objValue) {
             return f(objValue);
-          } else if (objValue.startDate) {
+          } else if (objValue && objValue.startDate) {
             return [f(objValue.startDate), f(objValue.endDate)].join(opts.locale.separator);
           } else {
             return '';
@@ -132,14 +132,15 @@
           var eventType, results;
           el.daterangepicker(angular.extend(opts, {
             autoUpdateInput: false
-          }), function(start, end) {
-            return $scope.$apply(function() {
-              return $scope.model = opts.singleDatePicker ? start : {
-                startDate: start,
-                endDate: end
-              };
-            });
-          });
+          }), el.on('apply.daterangepicker', function(ev, picker) {}), $scope.$apply(function() {
+            var newEndDate, newStartDate;
+            newStartDate = picker.startDate;
+            newEndDate = picker.endDate;
+            return $scope.model = opts.singleDatePicker ? newStartDate : {
+              startDate: newStartDate,
+              endDate: newEndDate
+            };
+          }));
           _picker = el.data('daterangepicker');
           results = [];
           for (eventType in opts.eventHandlers) {
